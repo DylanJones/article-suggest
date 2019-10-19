1d0501985fbbef27a7cd7d1dc41b69d1a680db93
Rishi Shyamala <rishis007@gmail.com>
2019-10-19 05:29:46 -0400

    add file

---
+++ text_parser_write.py
@@ -0,0 +1,95 @@
+import csv, sys, re, string,json
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from scipy.sparse import coo_matrix
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+def extract_results(feature_names, sorted_items):  
+    score_vals = []
+    feature_vals = []
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+        #keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+    #create a tuples of feature,score
+    #results = zip(feature_vals,score_vals)
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=score_vals[idx]
+    return results
+
+
+#get english stopwords from nltk
+from nltk.corpus import stopwords
+stopWords = set(stopwords.words('english'))
+
+#read the news articles and titles from dataset
+csvfile = open("all-the-news/articles1.csv")
+myText = csv.reader(csvfile)
+csv.field_size_limit(sys.maxsize)
+titles = []
+textList = []
+for item in myText:
+    titles.append(item[2])
+    textList.append(item[9].lower())
+# print(len(textList))
+textList.pop(0)
+titles.pop(0)
+
+#remove all unnecessary words from dataset
+pattern = re.compile('[^a-z A-Z]')
+cleanText = []
+for i, item in enumerate(textList):
+    print(i)
+    item = pattern.sub('', item)
+    lem = WordNetLemmatizer()
+    text = [lem.lemmatize(word) for word in item.split() if word not in stopWords]
+    text = " ".join(text)
+    cleanText.append(text)
+
+#vectorize words
+print("vectorizing words")
+cv=CountVectorizer(max_df=0.85,stop_words=list(stopWords), max_features=10000, ngram_range=(1,3))
+print("fitting to model")
+X=cv.fit_transform(cleanText)
+
+#tf-idf calculation
+print("transform tfidf to model")
+tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+print("fit model to tfidf values")
+tfidf_transformer.fit(X)
+print("getting feature names")
+feature_names=cv.get_feature_names()
+tfidf_lists = []
+count = 0
+#tf-idf convert to keywords ranking
+for tfidf_calc in cleanText:
+    print(count)
+    count += 1
+    tf_idf_vector=tfidf_transformer.transform(cv.transform([tfidf_calc]))
+    sorted_items=sort_coo(tf_idf_vector.tocoo())
+    keywords=extract_results(feature_names,sorted_items)
+    tfidf_lists.append(keywords)
+
+titles_file = open("title.txt", "w")
+#article_file = open("article.txt", "w")
+results_file = open("keywords.txt", "w")
+
+#save to file
+#for q in range(len(titles)):
+#    titles_file.write(titles[q])
+#    #article_file.write(cleanText[q])
+#    results_file.write(tfidf_lists[q])
+titles_file.write(json.dumps(titles))
+results_file.write(json.dumps(tfidf_lists))
+
+
+print("len(titles)", len(titles))# list
+print("len(cleanText)", len(cleanText))#print length
+print("len(tfidf_lists)", len(tfidf_lists))#list of dicts
+
